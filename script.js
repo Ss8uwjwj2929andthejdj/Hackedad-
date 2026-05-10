aee2826943bcb6663a7000b8c631127bd34eda81
Ss8uwjwj2929andthejdj <58967862+Ss8uwjwj2929andthejdj@users.noreply.github.com>
2026-05-10 16:30:06 -0600

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// Funcionalidad de CineGo
-
-function seleccionarPlan(plan) {
-    // Simulación de proceso de suscripción
-    alert("Has seleccionado el Plan " + plan + ". ¡La función está por comenzar!");
-    console.log("Iniciando suscripción para: " + plan);
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("CineGo listo para el streaming.");
-});
 function seleccionarPlan(plan) {
     // Muestra el formulario de pago
     document.getElementById('area-pago').style.display = 'block';
